c0ad1abae2dfced8dcf44c49e80ca13365139767
Gema Giavari <52576980+gemagiavari@users.noreply.github.com>
2024-06-18 13:02:11 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#form');
+const input = document.querySelector('input');
+const list = document.querySelector('#notes');
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const noteValue = input.value.toLowerCase();
+
+    const newList = document.createElement('li');
+    newList.innerText = noteValue;
+    console.log(newList);
+
+    list.append(newList);
+    input.value = '';
+
+
+})
